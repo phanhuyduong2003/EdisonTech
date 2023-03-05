0a681b609686b5e6ec96b87d76fe5fd6a478dae2
phanhuyduong2003 <huyduongphan8@gmail.com>
2023-03-05 21:20:13 +0700

    exercise

--- B5/BT/BT/exercise-5/exercise-5.js
+++ B5/BT/BT/exercise-5/exercise-5.js
@@ -1,5 +1,12 @@
 function checkUsersValid(goodUsers) {
   return function allUsersValid(submittedUsers) {
-    // Coding here
+    return submittedUsers.every(function (submittedUsers) {
+      return goodUsers.some(function (goodUsers) {
+        return goodUsers.id == submittedUsers.id
+      })
+    })
   };
 }
+let goodUsers = [{ id: 1 }, { id: 2 }, { id: 3 }];
+let testAllValid = checkUsersValid(goodUsers);
+console.log(testAllValid([{ id: 2 }, { id: 1 }]));
